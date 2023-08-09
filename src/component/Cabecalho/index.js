b3dc4f5dc46b6cf5693e28c1e97dd07f56ece32f
sophia <ferreirasophia056@gmail.com>
2023-08-09 08:59:38 -0300

    nao termi8nei

---
+++ src/component/Cabecalho/index.js
@@ -0,0 +1,13 @@
+import './index.scss';
+
+
+export default function Cabecalho() {
+  
+  return (
+    <header className='comp-cabecalho'>
+        <img className='logo' src='/assets/images/logo.png' alt='' />   
+        
+  
+    </header>
+  )
+}
